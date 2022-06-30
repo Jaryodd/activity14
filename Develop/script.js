21991fd5b3c32dc733b1e5a695c39dd5222f3d77
Jaryodd Carter <jaryodd@gmail.com>
2022-06-30 17:13:48 -0600

    finished

---
+++ Develop/script.js
@@ -0,0 +1,36 @@
+$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm'));
+
+$(".savebtn").on("click", function () {
+
+    var textArea = $(this).siblings(".description").val()
+    var divKey = $(this).parent().attr("id")
+
+    localStorage.setItem(divKey, textArea)
+
+});
+
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+
+var currentTime = moment().hours();
+
+$(".time-block").each(function () {
+    var divNumber = $(this).attr("id")
+    if (divNumber<currentTime) {
+        $(this).addClass("past")
+    }
+    if (divNumber==currentTime) {
+        $(this).addClass("present")
+    }
+    if (divNumber>currentTime) {
+        $(this).addClass("future")
+    }
+
+})
